21282bc8cf0edea05009174c65bd128bdf32f6fd
xiaoyi <cooode@qq.com>
2015-04-04 22:35:39 +0800

    exit app after window closed

--- Nesting/Nesting/AppDelegate.m
+++ Nesting/Nesting/AppDelegate.m
@@ -15,12 +15,14 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-    
 }
 
+
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
 }
 
 @end
